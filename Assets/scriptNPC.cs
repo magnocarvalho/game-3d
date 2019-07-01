a08f0f205ab7caeccd7e6e3229872b04ae5167a5
Magno Carvalho <magnocarv@hotmail.com>
2019-07-01 19:24:10 -0300

    jogo de tiro, quase funcionando

---
+++ Assets/scriptNPC.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class scriptNPC : MonoBehaviour
+{
+    public GameObject pc;
+    private NavMeshAgent agente;
+    public GameObject[] waypoints = new GameObject[4];
+    private int index = 0;
+    public float maxDist = 1;
+    private bool perseguindo = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agente = GetComponent<NavMeshAgent>();
+        proximo();
+    }
+
+    void proximo()
+    {
+        agente.destination = waypoints[index++].transform.position;
+        if (index >= waypoints.Length)
+            index = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //agente.destination = pc.transform.position;
+        //Se a distância entre o NPC e o waypoint atual for menor que 1
+        //então, vá para o próximo destino
+        if (perseguindo || Vector3.Distance(transform.position, pc.transform.position) <= 5)
+        {
+            perseguindo = true;
+            agente.destination = pc.transform.position;
+        }else if (Vector3.Distance(transform.position, agente.destination) <= maxDist)
+            proximo();
+        
+    }
+}
--- Assets/scriptPC.cs
+++ Assets/scriptPC.cs
@@ -7,12 +7,21 @@ public class scriptPC : MonoBehaviour
     public float velocidade;
     public float velRot;
     private Rigidbody rbd;
+    public LayerMask mascara;
+    private AudioSource som;
+    private Quaternion rotOriginal;
+    private float rotMX=0;
+    public Camera olhos;
+    
     // Start is called before the first frame update
     void Start()
     {
         velRot = 40;
         velocidade = 5;
         rbd = GetComponent<Rigidbody>();
+        som = GetComponent<AudioSource>();
+        
+        rotOriginal = transform.localRotation;
         
     }
 
@@ -22,7 +31,14 @@ public class scriptPC : MonoBehaviour
         float moveL = Input.GetAxis("Horizontal");
         float moveF = Input.GetAxis("Vertical");
 
-       
+        rotMX += Input.GetAxis("Mouse X");
+        
+
+        Quaternion lado = Quaternion.AngleAxis(rotMX,Vector3.up);
+        
+
+        transform.localRotation = rotOriginal * lado ;
+
         rbd.velocity = transform.TransformDirection(new Vector3(moveL * velocidade, rbd.velocity.y, moveF * velocidade));
 
         int dir = 0;
@@ -32,6 +48,20 @@ public class scriptPC : MonoBehaviour
             dir = 1;
 
         transform.Rotate(new Vector3(0,dir * velRot * Time.deltaTime,0));
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            som.Play();
+            RaycastHit hit;
+
+            if (Physics.Raycast(olhos.transform.position, olhos.transform.forward, out hit, 100, mascara))
+            {
+                Vector3 forca = transform.forward * 500;
+                hit.collider.GetComponent<Rigidbody>().AddForce(forca);
+            }
+            
+
+        }
 
     }
 }
